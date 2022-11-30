91f82c417625537d9fa21cbb73351afc587b9284
laludash <laludash6@gmail.com>
2022-11-30 17:57:37 +0530

    committed successfully

---
+++ JagahProjectApi/src/main/java/JagahProjectApi/JagahProjectApiApplication.java
@@ -0,0 +1,13 @@
+package JagahProjectApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JagahProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JagahProjectApiApplication.class, args);
+	}
+
+}
---
+++ JagahProjectApi/src/test/java/JagahProjectApi/JagahProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package JagahProjectApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JagahProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
